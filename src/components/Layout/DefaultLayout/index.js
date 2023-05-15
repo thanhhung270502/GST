50632c967e7a9d1ff48170ff4732c6460eccc857
phuonguyen11 <phuonguyen11112002@gmail.com>
2023-05-15 13:12:57 +0700

    fetch data in all pages

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,11 +1,103 @@
 import Header from './Header';
 import Footer from './Footer';
 
+import React, { useState, useEffect } from 'react';
+import { sendData } from '~/api/api';
+import { getTheLastData } from '~/api/api';
+
 function DefaultLayout({ children }) {
+    //begin fetch data
+    const AIO_FEED_ID = ['gst-humi', 'gst-light', 'gst-soil', 'gst-temp'];
+    const AIO_USERNAME = 'vienminhphuc';
+    const AIO_KEY = '';
+    const AIO_BASE_URL = 'https://io.adafruit.com/api/v2/';
+
+    const TIMEOUT_MS = 10000; // Timeout for waiting for new data in ms
+
+    const url_temp = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[3] + '/data';
+    const url_light = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[1] + '/data';
+    const url_soil = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[2] + '/data';
+    const url_humi = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[0] + '/data';
+    // --------------------------------- Real-time --------------------------------- //
+    const send = async (data) => {
+        await sendData({type: data.feed_key.slice(4),value: data.value, time: data.created_at});
+   };        
+   const check = async(data) => {
+    const res = await getTheLastData(data.feed_key.slice(4))
+    if(res === '') {send(data); throw "Succesfully add to database";}
+    res.time = res.time.replace('.000', '')
+    if(res.time === data.created_at)
+    {
+        throw Error("Database already had this row!")
+    }
+    else send(data);
+   };
+   useEffect(() => {
+    setInterval(async () => {
+         fetch(url_temp, {
+            headers: {
+                'X-AIO-Key': AIO_KEY,
+                'Content-Type': 'application/json',
+            },
+
+        })
+            .then((response) => response.json())
+            .then((data) => {   
+                check(data[0]);
+            })
+            .catch((error) => console.log(error));
+    }, TIMEOUT_MS);
+    setInterval(async () => {
+        fetch(url_light, {
+            headers: {
+                'X-AIO-Key': AIO_KEY,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                check(data[0]);
+            })
+            .catch((error) => console.log(error));
+    }, TIMEOUT_MS + 1000);
+
+    setInterval(async () => {
+        fetch(url_humi, {
+            headers: {
+                'X-AIO-Key': AIO_KEY,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                check(data[0]);
+            })
+            .catch((error) => console.log(error));
+    }, TIMEOUT_MS + 2000);
+    setInterval(async () => {
+         
+        fetch(url_soil, {
+            headers: {
+                'X-AIO-Key': AIO_KEY,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                check(data[0]);
+            })
+            .catch((error) => console.log(error));
+    }, TIMEOUT_MS + 3000);
+}, []);
+
+
+
+
     return (
         <div>
             <Header />
             <div className="content">{children}</div>   
+
             <Footer />
         </div>
     );
--- src/pages/View Current Climate/index.js
+++ src/pages/View Current Climate/index.js
@@ -12,8 +12,6 @@ import lightpng from '../../assets/images/light.png';
 import irri from '../../assets/images/irrigation.png';
 import soilpng from '../../assets/images/soil.png'
 import $ from 'jquery';
-import { sendData } from '~/api/api';
-import { getTheLastData } from '~/api/api';
 function toggleFan(valueFan) {
     const username = 'vienminhphuc';
     const feedKey = 'gst-fan';
@@ -36,51 +34,21 @@ function toggleFan(valueFan) {
 }
 
 function Climate() {
-    const staticAt = ['High', 'Normal', 'Low'];
     const [fan, setFan] = useState(0);
 
     const [temp, setTemp] = useState(22);
-    const [tempTimePrev, setTempTimePrev] = useState(22);
-    // const [modeTemp, setModeTemp] = useState('Automatic');
-    // const [staticTemp, setStaticTemp] = useState('Normal');
 
     const [light, setLight] = useState(1200);
-    const [lightTimePrev, setLightTimePrev] = useState(1200);
-    // const [modeLight, setModeLight] = useState('Automatic');
-    // const [staticLight, setStaticLight] = useState('Normal');
 
     const [humi, setHumi] = useState(56);
-    const [humiTimePrev, setHumiTimePrev] = useState(56);
-    // const [modeHumi, setModeHumi] = useState('Automatic');
-    // const [staticHumi, setStaticHumi] = useState('Normal');
-
     const [soil, setSoil] = useState(56);
 
-    const AIO_FEED_ID = ['gst-humi', 'gst-light', 'gst-soil', 'gst-temp'];
-    const AIO_USERNAME = 'vienminhphuc';
     const AIO_KEY = '';
-    const AIO_BASE_URL = 'https://io.adafruit.com/api/v2/';
-
     const TIMEOUT_MS = 10000; // Timeout for waiting for new data in ms
-
-    const url_temp = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[3] + '/data';
-    const url_light = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[1] + '/data';
-    const url_soil = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[2] + '/data';
-    const url_humi = AIO_BASE_URL + AIO_USERNAME + '/feeds/' + AIO_FEED_ID[0] + '/data';
-    // --------------------------------- Real-time --------------------------------- //
-    const send = async (data) => {
-        await sendData({type: data.feed_key.slice(4),value: data.value, time: data.created_at});
-   };        
-   const check = async(data) => {
-    const res = await getTheLastData(data.feed_key.slice(4))
-    if(res === '') {send(data); throw "Succesfully add to database";}
-    res.time = res.time.replace('.000', '')
-    if(res.time === data.created_at)
-    {
-        throw Error("Database already had this row!")
-    }
-    else send(data);
-   };
+    const url_temp =  "http://localhost:3000/climates/last/temp" ;
+    const url_light = "http://localhost:3000/climates/last/light" ;
+    const url_soil = "http://localhost:3000/climates/last/soil" ;
+    const url_humi =  "http://localhost:3000/climates/last/humi" ;
 
      useEffect(() => {
         setInterval(async () => {
@@ -93,12 +61,11 @@ function Climate() {
             })
                 .then((response) => response.json())
                 .then((data) => {   
-                    check(data[0]);
-                    setTemp(data[0].value);            
+                    setTemp(data.value);            
                 })
                 .catch((error) => console.log(error));
         }, TIMEOUT_MS);
-        setInterval(async() => {
+        setInterval(async () => {
             fetch(url_light, {
                 headers: {
                     'X-AIO-Key': AIO_KEY,
@@ -107,13 +74,12 @@ function Climate() {
             })
                 .then((response) => response.json())
                 .then((data) => {
-                    check(data[0]);
-                    setLight(data[0].value);
+                    setLight(data.value);
                 })
                 .catch((error) => console.log(error));
         }, TIMEOUT_MS + 1000);
 
-        setInterval(async() => {
+        setInterval(async () => {
             fetch(url_humi, {
                 headers: {
                     'X-AIO-Key': AIO_KEY,
@@ -122,12 +88,12 @@ function Climate() {
             })
                 .then((response) => response.json())
                 .then((data) => {
-                    check(data[0]);
-                    setHumi(data[0].value);
+                    setHumi(data.value);
                 })
                 .catch((error) => console.log(error));
         }, TIMEOUT_MS + 2000);
-        setInterval(async() => {
+        setInterval(async () => {
+             
             fetch(url_soil, {
                 headers: {
                     'X-AIO-Key': AIO_KEY,
@@ -136,8 +102,7 @@ function Climate() {
             })
                 .then((response) => response.json())
                 .then((data) => {
-                    check(data[0]);
-                    setSoil(data[0].value);
+                    setSoil(data.value);
                 })
                 .catch((error) => console.log(error));
         }, TIMEOUT_MS + 3000);
